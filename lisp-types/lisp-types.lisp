73ea3122191df8e607cac501b2adaa2b568ebe4d
Jim Newton <jnewton@lrde.epita.fr>
2016-12-09 11:02:30 +0100

    updated cmp-objects to work with packages, and to do a better job (non recursive) with lists.

--- lisp-types/lisp-types.lisp
+++ lisp-types/lisp-types.lisp
@@ -271,23 +271,30 @@ symbol _ somewhere (recursively)."
 	    nil)
 	   (t
 	    (cmp-objects (class-name (class-of a)) (class-name (class-of b))))))
-	((and (symbolp a) (symbolp b))
-	 (if (equal (symbol-package a) (symbol-package b))
-	     (string<= a b)
-	     (string<= (package-name (symbol-package a))
-		       (package-name (symbol-package b)))))
-	((and (stringp a) (stringp b))
-	 (string<= a b))
-	((and (numberp a) (numberp b))
-	 (<= a b))
-	((and (characterp a) (characterp b))
-	 (char<= a b))
-	((not (and (listp a) (listp b)))
-	 (error "cannot compare ~A ~A with ~A ~A" (class-of a) a (class-of b) b))
-	((equal (car a) (car b))
-	 (cmp-objects (cdr a) (cdr b)))
-	(t
-	 (cmp-objects (car a) (car b)))))
+        (t
+         (typecase a
+           (symbol
+            (if (equal (symbol-package a) (symbol-package b))
+                (string<= a b)
+                (cmp-objects (symbol-package a)
+                             (symbol-package b))))
+           (string
+            (string<= a b))
+           (number
+            (<= a b))
+           (character
+            (char<= a b))
+           (package
+            (cmp-objects (package-name a)
+                         (package-name b)))
+           (list
+            ;; compare the first two elements which are not equal
+            (while (equal (car a) (car b))
+              (pop a)
+              (pop b))
+            (cmp-objects (car a) (car b)))
+           (t
+            (error "cannot compare ~A ~A with ~A ~A" (class-of a) a (class-of b) b))))))
 
 (defun alphabetize (patterns)
   "non-descructively sort a list of patterns into a canonical order."
