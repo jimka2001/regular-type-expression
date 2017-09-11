94af118fa7cc043131031f1e21f3519b7c50c640
Jim Newton <jnewton@lrde.epita.fr>
2017-09-11 14:14:31 +0200

    comment

--- lisp-types/decompose.lisp
+++ lisp-types/decompose.lisp
@@ -52,7 +52,6 @@
 	     (forget (type)
 	       (setf type-specifiers (remove type type-specifiers :test #'eq)))
 	     (remember (type)
-               ;;(format t "length=~D type=~A type-specifiers=~A~%" (length type-specifiers) type type-specifiers)
 	       (pushnew type type-specifiers :test #'equivalent-types-p)))
       (while type-specifiers
         (remove-disjoint)
